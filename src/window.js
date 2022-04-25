948231c2465cf5c9f86c50442c5e71a4e9313c54
Hodei Navarro <hodeinavarro13@gmail.com>
2022-04-25 18:08:58 +0200

    Use metaKey in addition to CtrlKey for click event
    On MacOS CtrlKey is used to simulate a right-click.
    
    issue11420
    review407311002

--- src/window.js
+++ src/window.js
@@ -1326,7 +1326,7 @@
             }).text(el_field.attr('name')).prepend(
                 Sao.common.ICONFACTORY.get_icon_img('tryton-drag')
             ).click(function(e) {
-                if (e.ctrlKey) {
+                if (e.ctrlKey || e.metaKey) {
                     node.toggleClass('bg-primary');
                 } else {
                     jQuery(e.target).addClass('bg-primary')
@@ -1350,7 +1350,7 @@
                     'field': parent_node[field].field,
                     'name': parent_node[field].name
                 }).text(name).click(e => {
-                    if(e.ctrlKey) {
+                    if (e.ctrlKey || e.metaKey) {
                         node.toggleClass('bg-primary');
                     } else {
                         this.fields_all.find('li').removeClass('bg-primary');
@@ -1711,7 +1711,7 @@
                 var node = jQuery('<li/>', {
                     'path': path
                 }).text(parent_node[name].string).click(e => {
-                    if(e.ctrlKey) {
+                    if (e.ctrlKey || e.metaKey) {
                         node.toggleClass('bg-primary');
                     } else {
                         this.fields_all.find('li')
@@ -1960,7 +1960,7 @@
                 'path': name,
                 'class': 'draggable-handle',
             }).text(long_string).click(function(e) {
-                if(e.ctrlKey) {
+                if (e.ctrlKey || e.metaKey) {
                     node.toggleClass('bg-primary');
                 } else {
                     jQuery(e.target).addClass('bg-primary')
