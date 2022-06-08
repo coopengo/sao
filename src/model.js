461e746d47e279ea53b263503dc540c563f8cca1
Cédric Krier <ced@b2ck.com>
2022-06-08 09:14:38 +0200

    Support string as value of Binary field
    issue11534
    review417311004
    (grafted from ec6b533425594469d272a625336d797539bad0c9)
    
    --HG--
    branch : 6.4

--- src/model.js
+++ src/model.js
@@ -2696,7 +2696,8 @@
         },
         get_size: function(record) {
             var data = record._values[this.name] || 0;
-            if (data instanceof Uint8Array) {
+            if ((data instanceof Uint8Array) ||
+                (typeof(data) == 'string')) {
                 return data.length;
             }
             return data;
@@ -2704,7 +2705,8 @@
         get_data: function(record) {
             var data = record._values[this.name] || [];
             var prm = jQuery.when(data);
-            if (!(data instanceof Uint8Array)) {
+            if (!(data instanceof Uint8Array) &&
+                (typeof(data) != 'string')) {
                 if (record.id < 0) {
                     return prm;
                 }
